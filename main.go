6a165dd632b656366a0fbdcd050fa9c8d724159c
mrmaye4 <mrmaye4@gmail.com>
2026-08-10 07:11:23 +0300

    feat: add new tables, migrations and requests

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
 import (
+	"database/sql"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/sinedviper/chirpy/internal/database"
+)
+
+import (
 	"encoding/json"
 	"fmt"
 	"log"
@@ -11,6 +22,7 @@ import (
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	queries        *database.Queries
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -21,7 +33,27 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func main() {
-	cfg := &apiConfig{}
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env:", err)
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	platform := os.Getenv("PLATFORM")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Error opening database:", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatal("Error connecting to database:", err)
+	}
+
+	queries := database.New(db)
+
+	cfg := &apiConfig{
+		queries: queries,
+	}
 
 	mux := http.NewServeMux()
 
@@ -38,8 +70,8 @@ func main() {
 			return
 		}
 
-		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
 	mux.HandleFunc("/admin/metrics", func(w http.ResponseWriter, r *http.Request) {
@@ -48,8 +80,8 @@ func main() {
 			return
 		}
 
-		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(fmt.Sprintf("<html>\n  <body>\n    <h1>Welcome, Chirpy Admin</h1>\n    <p>Chirpy has been visited %d times!</p>\n  </body>\n</html>", cfg.fileserverHits.Load())))
 	})
 	mux.HandleFunc("/admin/reset", func(w http.ResponseWriter, r *http.Request) {
@@ -58,13 +90,22 @@ func main() {
 			return
 		}
 
+		if platform != "dev" {
+			http.Error(w, "Method Not Found", http.StatusForbidden)
+			return
+		}
+
+		err := cfg.queries.RemoveUsers(r.Context())
+		if err != nil {
+			log.Println("Error removing users:", err)
+		}
 		cfg.fileserverHits.Store(0)
 
-		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(fmt.Sprintf("Hits: %d\n", cfg.fileserverHits.Load())))
 	})
-	mux.HandleFunc("/api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("POST /api/chirps", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 			return
@@ -73,7 +114,8 @@ func main() {
 		cfg.fileserverHits.Add(1)
 
 		type request struct {
-			Body string `json:"body"`
+			Body   string `json:"body"`
+			UserId string `json:"user_id"`
 		}
 
 		var req request
@@ -81,8 +123,8 @@ func main() {
 		err := json.NewDecoder(r.Body).Decode(&req)
 
 		if err != nil {
-			w.WriteHeader(500)
 			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(500)
 
 			type returnVal struct {
 				Error string `json:"error"`
@@ -98,8 +140,8 @@ func main() {
 		}
 
 		if len(req.Body) > 140 {
-			w.WriteHeader(400)
 			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(400)
 			type returnVal struct {
 				Error string `json:"error"`
 			}
@@ -130,14 +172,233 @@ func main() {
 			}
 		}
 
+		findUser, errFindUser := cfg.queries.FindUser(r.Context(), uuid.MustParse(req.UserId))
+		if errFindUser != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(404)
+			type returnVal struct {
+				Error string `json:"error"`
+			}
+			respBody := returnVal{
+				Error: "User not found",
+			}
+			resp, _ := json.Marshal(respBody)
+			w.Write(resp)
+			return
+		}
+
+		createChirp, errCreateChirp := cfg.queries.CreateChirp(r.Context(), database.CreateChirpParams{Body: req.Body, UserID: uuid.NullUUID{UUID: findUser.ID, Valid: true}})
+		if errCreateChirp != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(500)
+			type returnVal struct {
+				Error string `json:"error"`
+			}
+			respBody := returnVal{
+				Error: "Something went wrong with create a chirp",
+			}
+			resp, _ := json.Marshal(respBody)
+			w.Write(resp)
+			return
+		}
+
 		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(200)
+		w.WriteHeader(201)
 		type returnVal struct {
-			CleanedBody string `json:"cleaned_body"`
+			ID        string    `json:"id"`
+			CreatedAt time.Time `json:"created_at"`
+			UpdatedAt time.Time `json:"updated_at"`
+			Body      string    `json:"body"`
+			UserId    string    `json:"user_id"`
 		}
 
 		respBody := returnVal{
-			CleanedBody: strings.Join(cleanedBody, " "),
+			ID:        createChirp.ID.String(),
+			CreatedAt: createChirp.CreatedAt,
+			UpdatedAt: createChirp.UpdatedAt,
+			Body:      createChirp.Body,
+			UserId:    req.UserId,
+		}
+
+		resp, _ := json.Marshal(respBody)
+		w.Write(resp)
+	})
+
+	mux.HandleFunc("GET /api/chirps", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		cfg.fileserverHits.Add(1)
+
+		getChirps, errGetChirps := cfg.queries.GetChirps(r.Context())
+		if errGetChirps != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(404)
+			type returnVal struct {
+				Error string `json:"error"`
+			}
+			respBody := returnVal{
+				Error: "Can't get chirps",
+			}
+			resp, _ := json.Marshal(respBody)
+			w.Write(resp)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		type returnVal struct {
+			ID        string    `json:"id"`
+			CreatedAt time.Time `json:"created_at"`
+			UpdatedAt time.Time `json:"updated_at"`
+			Body      string    `json:"body"`
+			UserId    string    `json:"user_id"`
+		}
+
+		var respBody []returnVal
+
+		for _, chirp := range getChirps {
+			respBody = append(respBody, returnVal{
+				ID:        chirp.ID.String(),
+				CreatedAt: chirp.CreatedAt,
+				UpdatedAt: chirp.UpdatedAt,
+				Body:      chirp.Body,
+				UserId:    chirp.UserID.UUID.String(),
+			})
+		}
+
+		resp, _ := json.Marshal(respBody)
+		w.Write(resp)
+	})
+
+	mux.HandleFunc("GET /api/chirps/{chirpID}", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		cfg.fileserverHits.Add(1)
+
+		chirpID, err := uuid.Parse(r.PathValue("chirpID"))
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+
+			type returnVal struct {
+				Error string `json:"error"`
+			}
+
+			respBody := returnVal{
+				Error: "Invalid chirpID",
+			}
+
+			resp, _ := json.Marshal(respBody)
+			w.Write(resp)
+			return
+		}
+
+		getChirp, errGetChirp := cfg.queries.GetChirp(r.Context(), chirpID)
+		if errGetChirp != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(404)
+			type returnVal struct {
+				Error string `json:"error"`
+			}
+			respBody := returnVal{
+				Error: "Not found chirp",
+			}
+			resp, _ := json.Marshal(respBody)
+			w.Write(resp)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		type returnVal struct {
+			ID        string    `json:"id"`
+			CreatedAt time.Time `json:"created_at"`
+			UpdatedAt time.Time `json:"updated_at"`
+			Body      string    `json:"body"`
+			UserId    string    `json:"user_id"`
+		}
+
+		resp, _ := json.Marshal(returnVal{
+			ID:        getChirp.ID.String(),
+			CreatedAt: getChirp.CreatedAt,
+			UpdatedAt: getChirp.UpdatedAt,
+			Body:      getChirp.Body,
+			UserId:    getChirp.UserID.UUID.String(),
+		})
+		w.Write(resp)
+	})
+
+	mux.HandleFunc("/api/users", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		cfg.fileserverHits.Add(1)
+
+		type request struct {
+			Email string `json:"email"`
+		}
+
+		var req request
+
+		err := json.NewDecoder(r.Body).Decode(&req)
+
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(500)
+
+			type returnVal struct {
+				Error string `json:"error"`
+			}
+
+			respBody := returnVal{
+				Error: "Something went wrong",
+			}
+
+			resp, _ := json.Marshal(respBody)
+			w.Write(resp)
+			return
+		}
+
+		userCreate, errCreate := cfg.queries.CreateUser(r.Context(), sql.NullString{
+			String: req.Email,
+			Valid:  true,
+		})
+
+		if errCreate != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(500)
+			type returnVal struct {
+				Error string `json:"error"`
+			}
+			respBody := returnVal{
+				Error: "Something went wrong",
+			}
+			resp, _ := json.Marshal(respBody)
+			w.Write(resp)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(201)
+		type returnVal struct {
+			ID        string    `json:"id"`
+			CreatedAt time.Time `json:"created_at"`
+			UpdatedAt time.Time `json:"updated_at"`
+			Email     string    `json:"email"`
+		}
+
+		respBody := returnVal{
+			ID:        userCreate.ID.String(),
+			CreatedAt: userCreate.CreatedAt,
+			UpdatedAt: userCreate.UpdatedAt,
+			Email:     userCreate.Email.String,
 		}
 
 		resp, _ := json.Marshal(respBody)
